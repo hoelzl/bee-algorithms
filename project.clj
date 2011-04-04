90e8f6ad60ab447af02118eb7669df04c1ba4bb5
Matthias Hoelzl <tc@xantira.com>
2011-04-04 23:23:14 +0200

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject test-01 "0.1.0-SNAPSHOT"
+  :description "A demonstration of some bee algorithms."
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [incanter "1.2.3"]]
+  :dev-dependencies [[swank-clojure "1.3.0"]])
---
+++ src/beeAlgorithms/core.clj
@@ -0,0 +1,22 @@
+(ns beeAlgorithms.core
+  (:use (incanter core stats charts)))
+
+(def $pi java.lang.Math/PI)
+
+(defn accelerated-sin-2 [x]
+  (let [scaled-x (/ x $pi 10.0)]
+    (sin (* $pi scaled-x scaled-x))))
+
+(defn accelerated-sin-3 [x]
+  (let [scaled-x (/ x $pi 10.0)]
+    (sin (* 0.3 $pi scaled-x scaled-x scaled-x))))
+
+(defn accelerated-sin-exp [x]
+  (let [scaled-x (/ x $pi 10)]
+    (sin (- (exp scaled-x) 1))))
+
+
+;; (view (function-plot accelerated-sin-2 0 120.0))
+;; (view (function-plot accelerated-sin-3 0 120.0))
+;; (view (function-plot accelerated-sin-exp 0 120.0))
+
