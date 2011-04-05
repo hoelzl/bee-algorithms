bc46ee60251f38331e43834dac56be6879a27f40
Matthias Hoelzl <tc@xantira.com>
2011-04-05 03:07:51 +0200

    Changed plot type to x/y-plot.

--- src/beeAlgorithms/core.clj
+++ src/beeAlgorithms/core.clj
@@ -16,9 +16,16 @@
   (let [scaled-x (/ x $pi 10)]
     (sin (- (exp scaled-x) 1))))
 
+(defn external-temperature [t]
+  (* 5 (accelerated-sin-exp t)))
+
+(defn with-delay [sequence]
+  (cons (first sequence) sequence))
 
 (defn -main [& args]
-  (view (function-plot accelerated-sin-2 0 120.0))
-  (view (function-plot accelerated-sin-3 0 120.0))
-  (view (function-plot accelerated-sin-exp 0 120.0))
+  (let [time (range 0 120 0.1)
+        temperature (map external-temperature time)]
+  (view (xy-plot time temperature)))
+  ; (view (function-plot accelerated-sin-3 0 120.0))
+  ; (view (function-plot accelerated-sin-exp 0 120.0))
 )
